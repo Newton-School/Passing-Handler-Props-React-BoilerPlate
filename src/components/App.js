1404bec46cc1cae5b45c8edafa33e98dee70862a
Shreya Chandra <shreya.chandra96@gmail.com>
2020-11-22 19:21:38 +0530

    Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,50 @@
 import React, { useState } from "react";
-// import * as React from 'react';
 import '../styles/App.css';
-import Child from './Child'
-const App = () => {
-  let [currentBackground, setCurrentBackground] = useState({ background: "" })
-  const changeColor = (childStyleHandler) => {
-    childStyleHandler(currentBackground)
+import Selection from './Selection';
+import ColourSelector from './ColourSelector';
+
+const colourConfig = [{
+    key: 'blue',
+    label: 'Blue',
+    classname: 'btn-blue',
+    background: 'rgb(34, 193, 195)'
+  }, {
+    key: 'orange',
+    label: 'Orange',
+    classname: 'btn-orange',
+    background: 'rgb(221, 112, 18)'
+  }, {
+    key: 'green',
+    label: 'Green',
+    classname: 'btn-green',
+    background: 'rgb(44, 209, 88)'
   }
+]
+
+const title = 'Select the gradient and then the Box to change the color';
+
+const App = () => {
+  let [nextBackground, selectNextBackground] = useState({ nextBackground: "" })
+  const applyColor = (updateSelectionStyle) => {
+    updateSelectionStyle(nextBackground)
+  }
+
   return (
     <div id="master">
-      <h1 className="heading"> Parent</h1>
-      <h5 className="heading"> Select the gradient then the Box to change color</h5>
+      <h5 className="heading">{/* display title here */}</h5>
 
-      <div className="holder">
-        <button className='btn-blue' onClick={() => { setCurrentBackground({ background: 'rgb(34, 193, 195)' }) }}> Blue</button>
-        <button className='btn-orange' onClick={() => { setCurrentBackground({ background: 'rgb(221, 112, 18)' }) }}>Orange</button>
-        <button className='btn-green' onClick={() => { setCurrentBackground({ background: 'rgb(44, 209, 88)' }) }}>Green</button>
+      <div className="row">
+        {colourConfig.map((config, index) => (
+          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
+        ))}
       </div>
 
-      <div className='holder child-holder' id="children-wrapper">
-        <Child changeColor={changeColor} />
-        <Child changeColor={changeColor} />
-        <Child changeColor={changeColor} />
+      <div className='row' id="children-wrapper">
+        {
+          ["selection1", "selection2", "selection3"].map(key => (
+            <Selection key={key} applyColor={applyColor} />
+          ))
+        }
       </div>
     </div >
   )
